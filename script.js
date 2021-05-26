3304836272662dfb51cae2676486d572746aae9d
Andrew Grisom <andrewg3501@hstat.org>
2021-05-26 04:09:52 +0000

    added js and separate pages for the interface

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("hello, welcome to the site")
+alert("please keep in mind this is a beta, so there may be bugs here and there where things don't go as intended")
+var name = prompt("what is your name")
+alert("welcome " + name + ", we hope you enjoy your stay here")
+
+// JS
