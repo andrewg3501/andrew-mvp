fc58b396f0b187f5bdfe0d9f3564cac9592bd54a
Andrew Grisom <andrewg3501@hstat.org>
2021-06-01 05:35:37 +0000

    fixed a bunch of code

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-alert("hello, welcome to the site")
-alert("please keep in mind this is a beta, so there may be bugs here and there where things don't go as intended")
-var name = prompt("what is your name")
-alert("welcome " + name + ", we hope you enjoy your stay here")
 
-// JS
+
